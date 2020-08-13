e9b005f644dfded782a46af7432c127bd323e7c1
bravopy <humzahasan0126@gmail.com>
2020-08-13 20:17:33 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['']);
+
+  const sndMsg = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  };
+
   return (
-    <div className="App">
-     <h1>Let's Go!</h1>
+    <div className='App'>
+      <h1>Let's Go!</h1>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        />
+        <button onClick={sndMsg}>Send Message</button>
+      </form>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
