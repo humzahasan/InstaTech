989ae32cb5f77e931ce4834b74980d6fd7cfa596
bravopy <humzahasan0126@gmail.com>
2020-08-14 06:39:13 +0530

    Message Component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
+import {Button, FormControl, InputLabel, Input} from '@material-ui/core';
+import Message from './Message';
+//import SendIcon from '@material-ui/icons/Send';
 
 function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState(['']);
-
+  
   const sndMsg = (event) => {
     event.preventDefault();
     setMessages([...messages, input]);
@@ -15,16 +18,27 @@ function App() {
     <div className='App'>
       <h1>Let's Go!</h1>
       <form>
-        <input
-          value={input}
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-        />
-        <button onClick={sndMsg}>Send Message</button>
+        <FormControl>
+          <InputLabel>Send Message</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+          />
+        </FormControl>
+        <Button
+          variant='contained'
+          type='submit'
+          color='secondary'
+          disabled={!input}
+          onClick={sndMsg}
+        >
+          Send Message
+        </Button>
       </form>
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message text={message}/>
       ))}
     </div>
   );
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message(props) {
+    return (
+        <div>
+            <p>{props.text}</p>
+        </div>
+    )
+}
+
+export default Message
